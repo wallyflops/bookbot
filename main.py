1a888cef2a65e192bbb2be1b325cccfb324c2b27
James Waller <waller.jc@gmail.com>
2025-01-26 19:37:20 +0000

    progress

--- main.py
+++ main.py
@@ -15,9 +15,21 @@ def count_words_in_book(book_text):
 
 def count_letters(book_text):
     letters = {}
-    for letter in book_text:
-        if letter not in letters:
-            letters[letter] += 1
     
-    print(letters)
+    for letter in book_text: 
+        lower_letter = letter.lower()
+        if lower_letter in 'abcdefghijklmnopqrstuvwxyz ':
+            if lower_letter not in letters:
+                letters[lower_letter] = 1
+            else:
+                letters[lower_letter] += 1
+    
+    print('{', end='')
+    output_strings = []
+    for letter in letters:
+        output_strings.append(f"'{letter}': {letters[letter]}")
+    print(', '.join(output_strings), end='')
+    print('}')
+
+
 main()
